fbdccba6cfc9d56905038fc6505443ce6c966824
Alaa Mohammed A Alhelal <alalhelal@elm.sa>
2022-11-01 10:27:50 +0300

    Combining Reducers

--- src/components/CommentFormComponent.js
+++ src/components/CommentFormComponent.js
@@ -1,94 +1,12 @@
-import React, {Component} from 'react';
-import { Card, CardImg, CardTitle, CardText, CardBody, Breadcrumb, BreadcrumbItem, Col, Row, Label, Button, Modal, ModalBody, ModalHeader } from 'reactstrap';
-import { Link } from 'react-router-dom';
+import React, { Component } from 'react';
+import { Col, Row, Label, Button, Modal, ModalBody, ModalHeader } from 'reactstrap';
 import { Control, LocalForm, Errors } from 'react-redux-form';
-
-
-
-function RenderComments({ comments }) {
-    if (comments != null) {
-        const allComents = comments.map((comment) => {
-            return (
-                <ul className="list-unstyled">
-                    <li>{comment.comment}</li>
-                    <li>-- {comment.author} , {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit' }).format(new Date(Date.parse(comment.date)))}</li>
-                </ul>
-            );
-        });
-
-        return (
-            <div className="col-sm-12 col-md-5 m-1">
-                <h4>Comments</h4>
-                {allComents}
-            </div>
-        );
-
-    }
-    else {
-        return (<div></div>);
-    }
-}
-
-
-function RenderDish({ dish }) {
-    return (
-        <Card>
-            <CardImg width="100%" object src={dish.image} alt={dish.name} />
-            <CardBody>
-                <CardTitle><h5>{dish.name}</h5></CardTitle>
-                <CardText>{dish.description}</CardText>
-            </CardBody>
-        </Card>
-    );
-}
-
-
-
-
-const DishDetails = (props) => {
-    if (props.dish != null) {
-        return (
-            <div className="container" >
-                <div className="row">
-                    <Breadcrumb>
-                        <BreadcrumbItem><Link to='/home'>Home</Link></BreadcrumbItem>
-                        <BreadcrumbItem><Link to='/menu'>Menu</Link></BreadcrumbItem>
-                        <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
-                    </Breadcrumb>
-                    <div className="col-12">
-                        <h3>{props.dish.name}</h3>
-                        <hr />
-                    </div>
-                </div>
-
-                <div className="row">
-                    <div className="col-12 col-md-5 m-1">
-                        <RenderDish dish={props.dish} />
-                    </div>
-                    <div className="col-12 col-md-5 m-1">
-                        <RenderComments comments={props.comments} />
-                        <CommentForm />
-                    </div>
-                </div>
-            </div>
-        );
-    }
-    else {
-        return (
-            <div></div>
-        );
-    }
-}
-
-
-export default DishDetails;
 
 
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
 const minLength = (len) => (val) => val && (val.length >= len);
 
-
-export class CommentForm extends Component {
+class CommentForm extends Component {
 
     constructor(props) {
         super(props);
@@ -122,7 +40,10 @@ export class CommentForm extends Component {
                 <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
                     <ModalHeader toggle={this.toggleModal}>Submit Comment</ModalHeader>
                     <ModalBody>
+
                         <LocalForm onSubmit={(values) => this.handleSubmit(values)}>
+
+
                             <Col className="form-group">
                                 <Label htmlFor='rating' md={10}>Rating</Label>
                                 <Col md={12}>
@@ -135,6 +56,8 @@ export class CommentForm extends Component {
                                     </Control.select>
                                 </Col>
                             </Col>
+
+
                             <Col className="form-group">
                                 <Label htmlFor='author' md={10}>Your Name</Label>
                                 <Col md={12}>
@@ -154,6 +77,8 @@ export class CommentForm extends Component {
                                     />
                                 </Col>
                             </Col>
+
+
                             <Col className="form-group">
                                 <Label htmlFor='comment' md={10}>Comment</Label>
                                 <Col md={12}>
@@ -161,6 +86,8 @@ export class CommentForm extends Component {
                                         className="form-control" />
                                 </Col>
                             </Col>
+
+
                             <Row className="form-group mt-5">
                                 <Col md={{ size: 3, offset: 0}}>
                                     <Button type="submit" value="submit" color="primary">Submit</Button>
@@ -168,11 +95,15 @@ export class CommentForm extends Component {
                             </Row>
 
                         </LocalForm>
+
+
                     </ModalBody>
                 </Modal>
             </div>
+
         );
     }
 
 }
 
+export default CommentForm;
